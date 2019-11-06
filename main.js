7840c8e41ce8a922839a411feb41fae9917671b5
Rasheem Gilbert <rasheem.gilbert@gmail.com>
2019-11-06 14:07:41 -0500

    Converter

---
+++ main.js
@@ -0,0 +1,20 @@
+// Select input field
+const input = document.querySelector("#input");
+const convert = () => {
+  const output = document.querySelector("#output");
+
+  if (input.value !== "") {
+    output.style.display = "block";
+  } else {
+    output.style.display = "none";
+  }
+
+  // gram
+  document.querySelector("#gram").textContent = input.value * 453.592;
+  // kilogram
+  document.querySelector("#kilogram").textContent =
+    (input.value * 453.592) / 1000;
+  // ounce
+  document.querySelector("#ounce").textContent = input.value * 16;
+};
+input.addEventListener("input", convert);
